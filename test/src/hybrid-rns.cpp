4f9c51affb0f3b1bc080c6f9a94e3ac7cbdad948
Imre Angelo <imre@angelo.no>
2026-05-11 15:07:52 +0200

    External product

--- test/src/hybrid-rns.cpp
+++ test/src/hybrid-rns.cpp
@@ -2,8 +2,9 @@
 
 using namespace lbcrypto;
 
-// TODO: Move to context
+// TODO: Move everything to context class
 namespace hybrid {
+    /// @todo Store in context 
     struct HybridTables {
         std::shared_ptr<typename DCRTPoly::Params> paramsQP;
         std::shared_ptr<typename DCRTPoly::Params> paramsQ;
@@ -16,7 +17,6 @@ namespace hybrid {
         // ScaleDown-tabeller (QP -> Q)
         std::vector<NativeInteger> pInvModq;          // [P^-1] mod qi
     };
-
 
     /// @todo Store in context 
     HybridTables InitHybridTables(const CryptoContext<DCRTPoly>& cc) {
@@ -53,7 +53,6 @@ namespace hybrid {
         }
         return tables;
     }
-
 
     /// @brief Scales the input m by P,
     /// embedding the message into a larger space: Q -> QP
@@ -140,17 +139,78 @@ namespace hybrid {
 
         const auto params = std::dynamic_pointer_cast<CryptoParametersRNS>(cc->GetCryptoParameters());
         const auto paramsQ = params->GetElementParams();
-        const auto t = params->GetPlaintextModulus();
+        const NativeInteger t = params->GetPlaintextModulus();
         return input.ApproxModDown(paramsQ, params->GetParamsP(), params->GetPInvModq(),
                 params->GetPInvModqPrecon(), params->GetPHatInvModp(),
                 params->GetPHatInvModpPrecon(), params->GetPHatModq(),
                 params->GetModqBarrettMu(), params->GettInvModp(),
                 params->GettInvModpPrecon(), t, params->GettModqPrecon());
     }
+
+    /// @brief Encrypt RGSW ciphertext
+    /// @todo Refactor -> manual encryption in QP with DGG/DUG
+    std::vector<Ciphertext<DCRTPoly>> Encrypt(
+        const CryptoContext<DCRTPoly>& cc, const HybridTables& tables, 
+        const PublicKey<DCRTPoly>& publicKey, const Plaintext& m
+    ) {
+        DEBUG_TIMER("Encrypt RGSW");
+
+        const auto params = std::dynamic_pointer_cast<CryptoParametersRNS>(cc->GetCryptoParameters());
+        const auto paramsQP = params->GetParamsQP();
+
+        // Scale by P
+        DCRTPoly mPoly = m->GetElement<DCRTPoly>();
+        DCRTPoly mP = Power(cc, mPoly);
+
+        std::vector<Ciphertext<DCRTPoly>> rgsw;
+        for(size_t i = 0; i < 2; i++) {
+            // TODO: This contains no noise!!! (not secure in production)
+            DCRTPoly c0(paramsQP, Format::EVALUATION, true);
+            DCRTPoly c1(paramsQP, Format::EVALUATION, true);
+            
+            auto ct = std::make_shared<CiphertextImpl<DCRTPoly>>(cc);
+            ct->SetElements({c0, c1});
+            rgsw.push_back(ct);
+        }
+
+        // Z + mG = Z + P(m) in hybrid
+        rgsw[0]->GetElements()[0] += mP;
+        rgsw[1]->GetElements()[1] += mP;
+
+        return rgsw;
+    }
+
+    /// @brief External Product
+    Ciphertext<DCRTPoly> EvalExternalProduct(
+        const CryptoContext<DCRTPoly>& cc, const HybridTables& tables,
+        const Ciphertext<DCRTPoly>& rlwe, const std::vector<Ciphertext<DCRTPoly>>& rgsw
+    ) {
+        auto c = rlwe->GetElements();
+        c[0].SetFormat(Format::EVALUATION);
+        c[1].SetFormat(Format::EVALUATION);
+
+        const auto d0 = Decompose(cc, c[0], tables);
+        const auto d1 = Decompose(cc, c[1], tables);
+
+        DCRTPoly out0(tables.paramsQP, Format::EVALUATION, true);
+        DCRTPoly out1(tables.paramsQP, Format::EVALUATION, true);
+
+        // Rad 0 (multipliseres med d0)
+        out0 += (d0 * rgsw[0]->GetElements()[0]);
+        out1 += (d0 * rgsw[0]->GetElements()[1]);
+        out0 += (d1 * rgsw[1]->GetElements()[0]);
+        out1 += (d1 * rgsw[1]->GetElements()[1]);
+
+        auto result = rlwe->Clone();
+        result->GetElements()[0] = ApproxModDown(cc, out0);
+        result->GetElements()[1] = ApproxModDown(cc, out1);
+
+        return result;
+    }
 }
 
 TEST(HYBRID, main) {
-    const std::vector<int64_t> value{-2};
+    const std::vector<int64_t> value{2};
 
     const auto ps = params::Small<CryptoContextBGVRNS>();
     const auto cc = GenCryptoContext(ps);
@@ -161,10 +221,14 @@ TEST(HYBRID, main) {
     const auto keys = cc->KeyGen();
     
     const Plaintext pt = cc->MakeCoefPackedPlaintext(value);
-    DCRTPoly m = pt->GetElement<DCRTPoly>();
+    const auto tables = hybrid::InitHybridTables(cc); // TODO: Move to context
     
+    const auto params = std::dynamic_pointer_cast<CryptoParametersRNS>(cc->GetCryptoParameters());
+    ASSERT_EQ(params->GetPlaintextModulus(), 1 << 8);
+
+    // Gadget Property
     {
-        const auto tables = hybrid::InitHybridTables(cc);
+        const DCRTPoly m = pt->GetElement<DCRTPoly>();
 
         const DCRTPoly pm = hybrid::Power(cc, m);
         const DCRTPoly dm = hybrid::Decompose(cc, m, tables);
@@ -173,5 +237,21 @@ TEST(HYBRID, main) {
 
         const auto mm = hybrid::ApproxModDown(cc, mult);
         ASSERT_EQ(mm, m * m);
+    }
+
+    // External Product
+    {
+        const auto rgsw = hybrid::Encrypt(cc, tables, keys.publicKey, pt);
+        const auto rlwe = cc->Encrypt(keys.publicKey, pt);
+        const auto ext = hybrid::EvalExternalProduct(cc, tables, rlwe, rgsw);
+
+        Plaintext decrypted;
+        cc->Decrypt(keys.secretKey, ext, &decrypted);
+        decrypted->SetLength(value.size());
+
+        DEBUG_PRINT("External Product: " << decrypted);
+
+        const auto expected = cc->MakeCoefPackedPlaintext({value[0] * value[0]});
+        ASSERT_EQ(decrypted, expected);
     }
 }
