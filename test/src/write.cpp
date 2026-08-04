d4d1d7fc522b3c1f7b99939e99889bd4c4ebf802
Imre Angelo <imre@angelo.no>
2026-08-04 12:47:11 +0200

    Updated params comment

--- test/src/write.cpp
+++ test/src/write.cpp
@@ -97,7 +97,7 @@ TEST_P(Server, Write) {
 }
 
 // 2u, 16u, 32u
-INSTANTIATE_TEST_SUITE_P(Sizes, Server, ::testing::Values(2u),
+INSTANTIATE_TEST_SUITE_P(Sizes, Server, ::testing::Values(32u),
                          [](const auto& param_info) { return "N" + std::to_string(param_info.param); });
 
 }  // namespace spar::test
